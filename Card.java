88b231dc5decad812597a601f762d981e59f6054
ericgtheboss <71529824+ericgtheboss@users.noreply.github.com>
2021-04-22 23:16:50 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,32 @@
+public abstract class Card {
+
+	private int cardSuit;
+	private int cardNumber;
+
+	public void getCardSuit() {
+		// TODO - implement Card.getCardSuit
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cardSuit
+	 */
+	public void setCardSuit(int cardSuit) {
+		this.cardSuit = cardSuit;
+	}
+
+	public void getCardNumber() {
+		// TODO - implement Card.getCardNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cardNumber
+	 */
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+}
